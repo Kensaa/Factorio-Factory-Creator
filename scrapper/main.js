95e5e09b0ae89324aaf7143a89d782394d723210
Kensaa <kensadevs@gmail.com>
2022-01-31 06:49:15 +0100

    added item type to scrapping

--- scrapper/main.js
+++ scrapper/main.js
@@ -2,89 +2,92 @@ const puppeteer = require('puppeteer');
 const fs = require('fs')
 
 async function main(){
-    const browser = await puppeteer.launch({headless:false})
+    const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
-    await page.goto("https://wiki.factorio.com/Main_Page")
+    await page.goto("https://wiki.factorio.com/Main_Page");
 
 
     let links = await page.evaluate(()=>{
-        let links = []
+        let links = [];
         let $ = window.$;
-        let itemtabs = $('div[id="mw-content-text"] > div > table[style="width: 100%; border-spacing: 8px; margin-top: -8px;"] > tbody > tr > td > div[style="padding-left: 1em;"] > div > div').toArray()
-        itemtabs.shift()
+        let itemtabs = $('div[id="mw-content-text"] > div > table[style="width: 100%; border-spacing: 8px; margin-top: -8px;"] > tbody > tr > td > div[style="padding-left: 1em;"] > div > div').toArray();
+        itemtabs.shift();
 
         for(let tab of itemtabs){
             let rows = $(tab).find('div').toArray();
             for(let row of rows){
                 let items = $(row).find('div').toArray();
                 for(let item of items){
-                    let l = $(item).find('a')[0]
-                    links.push(l.href)
+                    let l = $(item).find('a')[0];
+                    links.push(l.href);
                 }
             }
         }
 
-        return links
+        return links;
     })
 
-    fs.writeFileSync('links.json',JSON.stringify(links,null,4))
-    let crafts = []
+    fs.writeFileSync('links.json',JSON.stringify(links,null,4));
+    let crafts = [];
     let i = 0;
-    //let link = links[64]
+    //let link = links[0]
     for(let link of links){
-        await page.goto(link,{timeout:0})
-        await wait(150)
+        await page.goto(link,{timeout:0});
+        await wait(150);
         let craft = await page.evaluate(()=>{
             let $ = window.$;
-            let infoDiv = $($('div[class="infobox"] > div[class="tabber tabberlive"] > div[class="tabbertab"] > table > tbody > tr')[1]).find('td')[0]
-            if(!infoDiv)return null
-            let infos = $(infoDiv).find('div[class="factorio-icon"]').toArray()
+            let infoDiv = $($('div[class="infobox"] > div[class="tabber tabberlive"] > div[class="tabbertab"] > table > tbody > tr')[1]).find('td')[0];
+            if(!infoDiv)return null;
+            let infos = $(infoDiv).find('div[class="factorio-icon"]').toArray();
             infos = infos.map(e=>{
-                let count = parseFloat($(e).find('div').text().trim())
-                let attributes = $(e).find('a')[0].attributes
-                let item = attributes.title.value.trim()
-                let id = attributes.href.value.trim().split('/').pop()
-                return {item,count,id}
+                let count = parseFloat($(e).find('div').text().trim());
+                let attributes = $(e).find('a')[0].attributes;
+                let item = attributes.title.value.trim();
+                let id = attributes.href.value.trim().split('/').pop();
+                return {item,count,id};
             })
-            let finalItem = infos.pop()
-            let time = infos.shift().count
+            let finalItem = infos.pop();
+            let time = infos.shift().count;
 
-            let pb = $('div[class="infobox"] > table > tbody ')[0]
+            let pb = $('div[class="infobox"] > table > tbody ')[0];
 
-            let subs = $(pb).find('tr').toArray()
-            let pbindex = null
-            let i = 0
-            for(let s of subs){
-                if(s.className == "border-top"){
-                    let title = $($(s).find('td')[0]).find('p').text().trim()
-                    if(title == "Produced by")pbindex = i
+            let infoLines = $(pb).find('tr').toArray();
+            function find(f,infoLines){
+                let pbindex = null;
+                let i = 0;
+                for(let s of infoLines){
+                    if(s.className == "border-top"){
+                        let title = $($(s).find('td')[0]).find('p').text().trim();
+                        if(title == f)pbindex = i;
+                    }
+                    i++;
                 }
-                i++
+                return pbindex;
             }
-            console.log(pbindex);
-            let machineName = 'None'
+            let pbindex = find('Produced by',infoLines);
+            let machineName = 'None';
             if(pbindex){
-                let lines = $(pb).find('tr').toArray()
-                let machines = $(lines[pbindex+1]).find('td > div').toArray()
-                let firstMachine = machines.shift()
-                machineName = $(firstMachine).find('a')[0].attributes.title.value.trim().replace('1',"").replace("Stone","").trim()
+                let machines = $(infoLines[pbindex+1]).find('td > div').toArray();
+                let firstMachine = machines.shift();
+                machineName = $(firstMachine).find('a')[0].attributes.title.value.trim().replace('1',"").replace("Stone","").trim();
             }
 
-            
+            let typeIndex = find('Prototype type',infoLines);
+            let type = 'None';
+            if(typeIndex){
+                type = $($(infoLines[typeIndex]).find('td')[1]).find('p > a').text();
+            }
 
-           
-
-            let obj = {...finalItem,time,needed:infos,producedIn:machineName}
+            let obj = {...finalItem,time,needed:infos,type,producedIn:machineName};
             console.log(obj);
-            return obj
-        })
-        if(craft){
-            crafts.push(craft)
-        }
+            return obj;
+        });
+
+        if(craft)crafts.push(craft);
         i++
         console.log(i+' of '+links.length+' done');
-        fs.writeFileSync('crafts.json',JSON.stringify(crafts,null,4))
+        fs.writeFileSync('crafts.json',JSON.stringify(crafts,null,4));
     }
 }
 
