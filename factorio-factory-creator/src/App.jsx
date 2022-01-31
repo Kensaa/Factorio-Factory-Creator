e2eef90c16f4f315c31243e25cca1f12c0fdf83a
Kensaa <kensadevs@gmail.com>
2022-01-31 06:58:53 +0100

    started testing react flow

--- factorio-factory-creator/src/App.jsx
+++ factorio-factory-creator/src/App.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import ReactFlow from 'react-flow-renderer';
 
 export default class App extends Component {
+
+  elements = [
+    {
+      id:'1',
+      type:'input',
+      data:{label:'node 1'},
+      position: { x: 250, y: 25 }
+    },
+    {
+      id:'2',
+      type:'output',
+      data:{label:'node 2'},
+      position: { x: 200, y: 125 }
+    },
+    {id:'e1-2',source:'1',target:'2',type:'smoothstep',animated:true}
+  ]
+
   render() {
     return(
-    <div>
-        test
-    </div>
+      <div style={{ height: 300 }}>
+        <ReactFlow elements={this.elements} />
+      </div>
     )
   }
 }
