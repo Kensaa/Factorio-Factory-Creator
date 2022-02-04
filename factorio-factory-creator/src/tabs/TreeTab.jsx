076782469e026635ee594884257588be2e0075c1
Kensaa <kensadevs@gmail.com>
2022-02-05 00:06:42 +0100

    ca marcheeeeee

--- factorio-factory-creator/src/tabs/TreeTab.jsx
+++ factorio-factory-creator/src/tabs/TreeTab.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import ReactFlow from 'react-flow-renderer'
+import ReactFlow, { Background } from 'react-flow-renderer'
 import { fetchCrafts } from '../utils/crafts'
 
 
 export default class TreeTab extends Component {
   constructor(props) {
     super(props)
-    this.state = { crafts: [], elements: [], toCraft: 'Satellite' }
+    this.state = { crafts: [], elements: [], toCraft: 'Assembling_machine_3' }
+    this.spaces = {
+      x: 200,
+      y: 200,
+      offsetX: 50,
+      offsetY: 50
+    }
+
     fetchCrafts().then((res) => {
       this.setState({ crafts: res }, this.init)
     })
@@ -45,18 +52,26 @@ export default class TreeTab extends Component {
 
 
 
-    let craft = []
 
     let startCraft = crafts.find((e) => (e.id === toCraft))
-    console.log(startCraft.id);
+    //console.log(startCraft.id);
 
     function findItem(id) {
       return crafts.find((e) => (e.id === id))
     }
 
+    let indexMap = {}
+    let craft = []
+
     function buildCraftTree(currentItem, currentIndex = 0, layer = 0) {
+      //init index map layer 
+      if(!indexMap[layer])indexMap[layer]=0
+      //console.log(indexMap);
+
+
       //stop condition
       if (startRessources.includes(currentItem)) return;
+
       //pushing the first craft (result) in the list
       if (craft.length === 0) {
         if (!craft[layer]) {
@@ -71,23 +86,25 @@ export default class TreeTab extends Component {
       }
 
       //adding currentItem's requirement to the layer above current item but with position of is parent 
-      let index = 0
       for (let neededItem of findItem(currentItem).needed) {
         if (!craft[layer + 1]) {
           craft[layer + 1] = []
+          indexMap[layer] = 0
         }
         craft[layer + 1].push(
           {
             ...neededItem,
-            //id:neededItem.id,
-            parent: { layer, index: currentIndex }
+            self: { layer: layer + 1, id: neededItem.id, index:indexMap[layer] },
+            parent: { layer, id: currentItem, index: currentIndex }
           })
-        buildCraftTree(neededItem.id, index, layer + 1)
-        index++
+        buildCraftTree(neededItem.id, indexMap[layer], layer + 1)
+        indexMap[layer] = indexMap[layer] + 1
       }
 
     }
+
     buildCraftTree(startCraft.id)
+
     console.log('res : ');
     console.log(craft);
 
@@ -96,16 +113,17 @@ export default class TreeTab extends Component {
     let l = 0
     for (let layer of craft) {
       let i = 0
+      /*layer = layer.sort((a, b) => (
+        a.parent.index - b.parent.index
+      ))  */
       for (let item of layer) {
         elements.push({
           id: 'n' + l + '-' + i,
           type: 'default',
-          data: { label: item.item },
+          data: { label: (item.count ? item.count : 1)+' x '+item.item },
           position: { x: i * 200 + 50, y: l * 200 + 50 }
         })
         if (!(i === 0 && l === 0)) {
-          console.log(item.parent.layer);
-          console.log(item.parent.index);
           elements.push({
             id: 'e' + i + ',' + l,
             source: 'n' + item.parent.layer + '-' + item.parent.index,
@@ -132,7 +150,9 @@ export default class TreeTab extends Component {
     return (
       <div className='container tab'>
         <div className='reactFlow'>
-          <ReactFlow elements={this.state.elements} nodesDraggable={true} />
+          <ReactFlow elements={this.state.elements} nodesDraggable={false}>
+            <Background />
+          </ReactFlow>
         </div>
       </div>
     )
